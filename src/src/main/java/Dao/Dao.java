a193c24ca87d138726fbcd36929ef97528dcc71d
LiisaVuorenmaa <liisa.kuisma@hotmail.fi>
2021-04-05 11:01:21 +0300

    uudet tiedostot
    uudet tiedostot

--- src/src/main/java/Dao/Dao.java
+++ src/src/main/java/Dao/Dao.java
@@ -1,5 +1,114 @@
-package Dao;
+package dao;
+
+import java.sql.DriverManager;
+
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+import data.Question;
+
+import java.sql.Connection;
 
 public class Dao {
+	private String url;
+	private String user;
+	private String pass;
+	private Connection conn;
+	
+	public Dao(String url, String user, String pass) {
+		this.url=url;
+		this.user=user;
+		this.pass=pass;
+	}
+	
+	public boolean getConnection() {
+		try {
+	        if (conn == null || conn.isClosed()) {
+	            try {
+	                Class.forName("com.mysql.jdbc.Driver").newInstance();
+	                
+	            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+	                throw new SQLException(e);
+	                
+	            }
+	            conn = DriverManager.getConnection(url, user, pass);
+	            
+	        }
+	        System.out.println("connected");
+	        return true;
+	        
+		}
+		catch (SQLException e) {
+			System.out.println(e.getMessage());
+			System.out.println("No connected");
+			return false;
+		}
+	}
+	public ArrayList<Question> readAllQuestion() {
+		ArrayList<Question> list=new ArrayList<>();
+		try {
+			Statement stmt=conn.createStatement();
+			ResultSet RS=stmt.executeQuery("select * from kysymykset");
+			System.out.println("kysymysarray");
+			while (RS.next()){
+				Question f=new Question();
+				f.setId(RS.getInt("KYSYMYS_ID"));
+				f.setWhatquestion(RS.getString("KYSYMYS"));
+				list.add(f);
+				System.out.println("kysymysarrayhin lisätty kysymykset");
+			}
+			return list;
+		}
+		catch(SQLException e) {
+			return null;
+		}
+	}
+	public ArrayList<Question> updateQuestion(Question f) {
+		try {
+			String sql="update kysymykset set kysymys=? where id=?";
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, f.getWhatquestion());
+			pstmt.setInt(2, f.getId());
+			pstmt.executeUpdate();
+			return readAllQuestion();
+		}
+		catch(SQLException e) {
+			return null;
+		}
+	}
+	public ArrayList<Question> deleteQuestion(String id) {
+		try {
+			String sql="delete from kysymykset where id=?";
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			pstmt.executeUpdate();
+			return readAllQuestion();
+		}
+		catch(SQLException e) {
+			return null;
+		}
+	}
 
+	public Question readQuestion(String id) {
+		Question f=null;
+		try {
+			String sql="select * from kysymykset where id=?";
+			PreparedStatement pstmt=conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			ResultSet RS=pstmt.executeQuery();
+			while (RS.next()){
+				f=new Question();
+				f.setId(RS.getInt("id"));
+				f.setWhatquestion(RS.getString("whatquestion"));
+			}
+			return f;
+		}
+		catch(SQLException e) {
+			return null;
+		}
+	}
 }
---
+++ src/src/main/webapp/jsp/myscriptfile.js
@@ -0,0 +1,6 @@
+
+
+
+if(document.showquestions.1.checked) {
+	document.write("Tekstiä, joka pitaisi tulostua, kun checkbox on valittu.");
+}
