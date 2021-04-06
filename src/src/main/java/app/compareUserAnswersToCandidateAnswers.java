7b04f616c7cc5fdb66be74ae50f3a572039c1e76
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-04-06 13:53:20 +0300

    Start with showing the information of the candidates

---
+++ src/src/main/java/app/compareUserAnswersToCandidateAnswers.java
@@ -0,0 +1,53 @@
+package app;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.Dao;
+import data.Candidates;
+
+@WebServlet("/compareUserAnswersToCandidateAnswers")
+public class compareUserAnswersToCandidateAnswers extends HttpServlet{
+	
+	private static final long serialVersionUID = 1L;
+	 Dao dao = null;
+	
+	@Override
+	public void init() {
+		dao=new Dao("jdbc:mysql://localhost:3306/vaalikone", "admin", "salasana");
+	}
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public compareUserAnswersToCandidateAnswers() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<Candidates> list=null;
+		if (dao.getConnection()) {
+			list=dao.readAllCandidates();
+		}
+		else {
+			System.out.println("No connection to database");
+		}
+	
+		request.setAttribute("candidateslist", list);
+
+		RequestDispatcher rd=request.getRequestDispatcher("/jsp/compareUserAnswersToCandidateAnswers.jsp");
+		rd.forward(request, response); 
+		
+	}	
+
+}
---
+++ src/src/main/webapp/jsp/compareUserAnswersToCandidateAnswers.jsp
@@ -0,0 +1,8 @@
+<%@ include file="../header.html" %>
+<head>
+<link rel="stylesheet" href="koodit.css">
+</head>
+
+	  <h1>Vertaa vastauksiasi kandidaattien vastauksiin </h1>
+		
+		<%@ include file="../footer.html" %>
