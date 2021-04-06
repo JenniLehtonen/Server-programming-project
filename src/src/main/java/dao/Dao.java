b8f2f34f4e0afabe9641127408a503902c92e8cf
JenniLehtonen <59826954+JenniLehtonen@users.noreply.github.com>
2021-04-06 09:30:30 +0300

    Muuttuuko dao-luokan alkukirjain?

--- src/src/main/java/dao/Dao.java
+++ src/src/main/java/dao/Dao.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 
-import data.Candidates;
+import data.Candidates; //korjaa virhe
 import data.Question;
 
 import java.sql.Connection;
